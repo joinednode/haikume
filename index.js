600298b524644b6ac9b927db7536a185535a7bac
Roger Stringer <roger@freekrai.net>
2015-10-23 18:14:49 -0600

    clean up

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var fs = require('fs');
 var adjectives = fs.readFileSync(__dirname + '/lib/adjectives.txt', 'utf8').split(' ');
 var nouns = fs.readFileSync(__dirname + '/lib/nouns.txt', 'utf8').split(" ");
-var adjLen = adjectives.length -1;
-var nounLen = nouns.length -1;
+var adjLen = adjectives.length - 1;
+var nounLen = nouns.length - 1;
 
 module.exports = function( stampme ) {
 	var stampme = (stampme ? true : false );
-	var unix = Math.round( +new Date() / 1000 ) + Math.floor( Math.random() * ( Math.round(+new Date() / 1000) ) );
-	var stamp = ( "" + unix ).slice(-5);
-	return adjectives[ randomInt(adjLen) ] + "-" + nouns[ randomInt(nounLen) ] + ( stampme ? "-" + stamp : '' );
+	return adjectives[ randomInt(adjLen) ] + "-" + nouns[ randomInt(nounLen) ] + ( stampme ? "-" + randomStamp() : '' );
 };
 
 function randomInt (max) {
 	return Math.floor(Math.random() * max);
+};
+
+function randomStamp(){
+	var unix = Math.round( +new Date() / 1000 ) + Math.floor( Math.random() * ( Math.round(+new Date() / 1000) ) );
+	return ( "" + unix ).slice(-5);	
 };
